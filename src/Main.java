4b5f1bc50b04cabd29cdbb1b7a58d7a871376c52
Engel62 <sr.engel@bk.ru>
2022-10-18 11:40:15 +0300

    задание 4 выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        System.out.println("Домашнее заданик 1");
+    double dog = 3;
+        System.out.println(dog);
+    dog = dog + 4;
+        System.out.println(dog);
+    dog =  dog - 3.5;
+        System.out.println(dog);
+    var cat = 3.6;
+        System.out.println(cat);
+    cat = cat + 4;
+        System.out.println(cat);
+    cat = cat - 1.6;
+        System.out.println(cat);
+    var paper = 763789;
+        System.out.println(paper);
+    paper = paper + 4;
+        System.out.println(paper);
+    paper = paper - 7639;
+        System.out.println(paper);}
 }
